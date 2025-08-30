874e49c4b10e44fda18023eeb8cc8b887878351e
hpriya07 <hemapriyavasudevan250704@gmail.com>
2025-08-30 06:22:30 -0700

    Update Book_Slots.java

--- Book_Slots.java
+++ Book_Slots.java
@@ -3,7 +3,7 @@ public class Book_Slots {
     private int booked_id;
 
     Book_Slots(Create_Slots obj) {
-        obj_book = obj;  // use the same Create_Slots instance
+        obj_book = obj; 
     }
 
     public void book() {
@@ -33,25 +33,20 @@ class Main1 {
     public static void main(String[] args) {
         Create_Slots parking = new Create_Slots(2);
 
-        // First person books
         Book_Slots person1 = new Book_Slots(parking);
         person1.book();
 
-        // Second person books
         Book_Slots person2 = new Book_Slots(parking);
         person2.book();
 
-        // Third person tries (should fail)
         Book_Slots person3 = new Book_Slots(parking);
         person3.book();
 
         System.out.println("Current slots: " + parking.getter_lots());
 
-        // Free a slot
         person1.free();
         System.out.println("After freeing, slots: " + parking.getter_lots());
 
-        // Another booking
         person3.book();
         System.out.println("Final slots: " + parking.getter_lots());
     }
