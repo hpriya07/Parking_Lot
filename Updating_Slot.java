f7c97b31b32e21c4fe7e28c68c7e3159873eae2e
hpriya07 <hemapriyavasudevan250704@gmail.com>
2025-08-30 06:23:00 -0700

    Update Updating_Slot.java

--- Updating_Slot.java
+++ Updating_Slot.java
@@ -10,7 +10,6 @@ public class Updating_Slot {
         obj_update.update_setter_free(update_id);
     }
 
-    // Book a new slot (ignores update_id, books first free slot)
     public void bookSlot() {
         int bookedId = obj_update.setter();
         if (bookedId == -1) {
@@ -22,7 +21,6 @@ class Unit_Testing_Update {
     public static void main(String[] args) {
         Create_Slots parking = new Create_Slots(3);
 
-        // Book using Book_Slots
         Book_Slots user1 = new Book_Slots(parking);
         user1.book(); // should book slot 0
 
@@ -31,12 +29,10 @@ class Unit_Testing_Update {
 
         System.out.println("Current slots: " + parking.getter_lots());
 
-        // Free slot 1 using Updating_Slot
         Updating_Slot updater = new Updating_Slot(1, parking);
         updater.freeSlot();
         System.out.println("After freeing slot 1: " + parking.getter_lots());
 
-        // Try booking a new slot with Updating_Slot
         Updating_Slot updater2 = new Updating_Slot(0, parking);
         updater2.bookSlot();
         System.out.println("After booking with updater: " + parking.getter_lots());
